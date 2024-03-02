210b764f761f4c98af559ba948ed41876432f733
Damayanti Ghosh <damayantighosh2@gmail.com>
2024-03-02 14:27:49 -0500

    success gcs to bq

--- data_engineering_project/gcp_etl/gcs_to_bq/udf.js
+++ data_engineering_project/gcp_etl/gcs_to_bq/udf.js
@@ -1,11 +1,11 @@
-function transform(line){
-    let values =  line.split(',');
-    let obj = new Object();
-    // obj.id = values[0];
-    obj.name  = values[0];
-    // obj.email = values[2];
-    // obj.age  =  values[3];
-    obj.city =  values[1];
-    let jsonString = JSON.stringify(obj)
-    return jsonString
-}
+function transform(line) {
+    var values = line.split(',');
+    var obj = new Object();
+    obj.id = values[0];
+    obj.name = values[1];
+    obj.email = values[2];
+    obj.age = values[3];
+    obj.city = values[4];
+    var jsonString = JSON.stringify(obj);
+    return jsonString;
+   }
